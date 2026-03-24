67b8fda5429f4cb93620807863d1bd6819cda737
Mohammed Ali <mohammed.ghazy1235@gmail.com>
2026-03-24 21:29:46 +0200

    Used strtok instead of scanf for better input handling

--- src/main.c
+++ src/main.c
@@ -25,7 +25,7 @@ typedef struct {
 } Table;
 
 typedef enum {META_COMMAND_SUCCESS, META_COMMAND_UNRECOGNIZED_COMMAND} MetaCommandResult;
-typedef enum {PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT, PREPARE_SYNTAX_ERROR} PrepareResult;
+typedef enum {PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT, PREPARE_SYNTAX_ERROR, PREPARE_STRING_TOO_LONG} PrepareResult;
 typedef enum {STATEMENT_INSERT, STATEMENT_SELECT} StatementType;
 typedef enum {EXECUTE_SUCCESS, EXECUTE_TABLE_FULL} ExecuteResult;
 typedef struct {
@@ -89,6 +89,30 @@ void* row_slot (Table* table, uint32_t row_num) {
     return page + byte_offset;
 }
 
+PrepareResult prepare_insert (InputBuffer* input_buffer, Statement* statement) {
+    statement->type = STATEMENT_INSERT;
+
+    char* keyword = strtok(input_buffer->buffer, " ");
+    char* id_string = strtok(NULL, " ");
+    char* username = strtok(NULL, " ");
+    char* email = strtok(NULL, " ");
+
+    if (id_string == NULL || username == NULL || email == NULL) {
+        return PREPARE_SYNTAX_ERROR;
+    }
+
+    int id = atoi(id_string);
+    if (strlen(username) >= COLUMN_USERNAME_SIZE) return PREPARE_STRING_TOO_LONG;
+    if (strlen(email) >= COLUMN_EMAIL_SIZE) return PREPARE_STRING_TOO_LONG;
+
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+
+    return PREPARE_SUCCESS;
+
+}
+
 ExecuteResult execute_insert (Statement* statement, Table* table) {
     if (table->num_rows >= TABLE_MAX_ROWS) {
         return EXECUTE_TABLE_FULL;
@@ -149,20 +173,7 @@ MetaCommandResult do_meta_command (InputBuffer* input_buffer, Table* table) {
 }
 
 PrepareResult prepare_statement (InputBuffer* input_buffer, Statement* statement) {
-    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
-        statement->type = STATEMENT_INSERT;
-        int args_assigned = 
-            sscanf(input_buffer->buffer, "insert %d %s %s ", 
-            &(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email);
-        if (args_assigned < 3) return PREPARE_SYNTAX_ERROR;
-        return PREPARE_SUCCESS;
-    }
-
-    if (strcmp(input_buffer->buffer, "select") == 0) {
-        statement->type = STATEMENT_SELECT;
-        return PREPARE_SUCCESS;
-    }
-    return PREPARE_UNRECOGNIZED_STATEMENT;
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0) return prepare_insert(input_buffer, statement);
 }
 
 ExecuteResult execute_statement (Statement* statement, Table* table) {
